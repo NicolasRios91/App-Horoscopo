91da7099499343dc4411278e2b20292bc3cc7d3f
riosnicolass <nicolasrios91@hotmail.com>
2022-08-16 19:34:44 -0300

    feat: error message

--- src/views/CardList/index.jsx
+++ src/views/CardList/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 import CardList from "../../models/cardList";
 import "./CardListView.css";
 const CardListView = () => {
+  const data = useSelector((state) => state.dataReducer);
   //TODO use effect in carlistView??
   // const data = useSelector((state) => state.dataReducer);
   // !data ? FecthApi() : void 0; //WTF?????
@@ -12,7 +13,8 @@ const CardListView = () => {
       <header>
         <h1>Tu Horóscopo</h1>
       </header>
-      <CardList />
+      {data?.length ?<CardList /> : <h3 style={{textAlign: 'center'}}>Error en la aplicacion</h3>}
+      
     </>
   );
 };
